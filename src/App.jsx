24c856ae7365e4f6095d1da6b47512883b9c8f1c
Sahil Singh Virdhi <sahilsv003@gmail.com>
2024-01-03 20:38:11 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const givenData = [
+    {
+      word: "React",
+      meaning: "A JavaScript library for building user interfaces.",
+    },
+    { word: "Component", meaning: "A reusable building block in React." },
+    { word: "State", meaning: "An object that stores data for a component." },
+  ];
+
+  const [dictionary, setDictionary] = useState(givenData);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleSearch = () => {
+    const searchTermLower = searchTerm.toLowerCase();
+    const findWord = dictionary.find(
+      (item) => item.word.toLowerCase() === searchTermLower
+    );
+
+    if (findWord) {
+      setResult(findWord.meaning);
+    } else {
+      setResult("Word not found in the dictionary.");
+    }
+  };
+
+  return (
+    <>
+      <header>Dictionary App</header>
+
+      <div>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="button" onClick={handleSearch}>
+          Search
+        </button>
+        <p> <b>Definition: </b> <br /> {result}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
